191f2369733a28f78009239cccfdd88e8a01c98c
ashutosh_k_akhouri <ashutosh.akhouri@gmail.com>
2015-09-11 13:54:26 +0530

    Angular pages - 1st checkin

---
+++ frontend/frontend/Scripts/app.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module('myApp', ['ngRoute']);
+app.controller('customersCtrl', function ($scope, $http) {
+    $http.get("http://www.w3schools.com/angular/customers.php")
+    .success(function (response) { $scope.names = response.records; });
+});
+
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when('/', 
+    {
+        controller: 'customersCtrl',
+        templateUrl: '/partials/main.html'
+    })
+    .when('/view2',
+        {
+            controller: 'customersCtrl',
+            templateUrl: '/partials/main2.html'
+        })
+});
